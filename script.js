0c31f735d25454a8ff52822f6fc6da7606043b2c
rockbikramdas <rockbikramdas@gmail.com>
2023-03-01 23:40:05 +0530

    improved nav ui

--- script.js
+++ script.js
@@ -2,14 +2,13 @@ const collapseFun =() =>{
     
     const element = document.getElementById("menuButton");
     var menuBar=document.getElementById('menuBar');
-    if(menuBar.className==='hidden lg:flex flex-col items-center lg:flex-row lg:callFUn() font-semibold'){
-        menuBar.className='flex lg:flex flex-col items-center lg:flex-row lg:callFUn() font-semibold'
+    if(menuBar.className==='hidden lg:flex flex-col items-center lg:flex-wrap lg:flex-row lg:w-full lg:justify-center  font-semibold'){
+        menuBar.className='flex lg:flex flex-col items-center lg:flex-wrap lg:flex-row lg:w-full lg:justify-center  font-semibold'
         element.innerHTML='<span class="material-symbols-outlined">close</span>';
     }
     else{
         element.innerHTML='<span class="material-symbols-outlined">menu</span>';
-        menuBar.className='hidden lg:flex flex-col items-center lg:flex-row lg:callFUn() font-semibold'
+        menuBar.className='hidden lg:flex flex-col items-center lg:flex-wrap lg:flex-row lg:w-full lg:justify-center  font-semibold'
     }
 
 }
-
